2c881c929ddd50a51e7a339e439cb87b387559b1
Tobias Widén <tobias.widen@trioptima.com>
2016-02-25 13:16:16 -0500

    Bye and welcome messages when users connect and disconnect

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  io.emit('user connect', 'welcome');
 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('user disconnect', 'bye');
   });
 
   socket.on('chat message', function(msg){
