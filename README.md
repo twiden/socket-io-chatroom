5514ae0668b40f24802deb55fac2671765e75386
Tobias Widén <tobias.widen@trioptima.com>
2016-02-25 14:39:23 -0500

    list active users

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ SocketIO Chat Application
 
 Following tutorial at http://socket.io/get-started/chat/
 
+Known bugs: Do not connect with the same nickname from two browsers. Do not connect with nickname *everyone*
+
 Extras:
 Message when users connect and disconnect
 Nicknames
 Show who is typing
+List connected users
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = {'everyone': true};
 var typers = {};
 
 app.get('/', function(req, res){
@@ -12,7 +13,9 @@ io.on('connection', function(socket){
   socket.emit('users typing', Object.keys(typers));
   socket.on('new user', function(nickname){
     name = nickname;
+    users[name] = true
     io.emit('user connect', 'Welcome ' + name)
+    io.emit('list users', Object.keys(users))
   });
 
   socket.on('typing', function(is_typing){
@@ -28,6 +31,8 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     io.emit('user disconnect', 'Bye ' + name);
     delete typers[name];
+    delete users[name];
+    io.emit('list users', Object.keys(users))
   });
 
   socket.on('chat message', function(msg){
